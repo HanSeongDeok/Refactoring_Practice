db323977df52854db81fad3aa3be73fc39b83826
sdhan <hsd970730@naver.com>
2023-07-02 21:41:04 +0900

    refactoring scenario2 day1

---
+++ src/main/java/org/example/refactoring2/Rental.java
@@ -0,0 +1,20 @@
+package org.example.refactoring2;
+
+// 추상클래스로 변경
+class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+}
