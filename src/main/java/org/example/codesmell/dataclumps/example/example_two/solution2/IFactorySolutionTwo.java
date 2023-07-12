70c0eb751a61810c9d318f6356d9a947cb75f994
sdhan <sdhan@suresofttech.com>
2023-07-12 12:54:33 +0900

    refactoring practice day3 -> remind

--- src/main/java/org/example/codesmell/dataclumps/example/example_two/solution2/IFactorySolutionTwo.java
+++ src/main/java/org/example/codesmell/dataclumps/example/example_two/solution2/IFactorySolutionTwo.java
@@ -13,9 +13,11 @@ public interface IFactorySolutionTwo{
      * 생성자에 필요한 setting 추상화를 구현한 객체 생성
      * @return
      */
+    // 팩토리 객체를 생성하는 IFactory 를 의존하여 팩토리 구현체를 생성한다.
     static IFactory<FactorySolutionTwoImpl> builderFactory(){
         return FactorySolutionTwoImpl::new;
     }
+    // 펙토리 메소드로 생성자를 생성한다.
     static IFactory<Solution_Two> solution_Two_Factory(FactorySolutionTwoImpl factorySolutionTwo){
         return () -> new Solution_Two(factorySolutionTwo);
     }
