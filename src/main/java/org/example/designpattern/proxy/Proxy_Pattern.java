bfba2ec9ac14d4702c3747fb534316734d1ed28a
sdhan <hsd970730@naver.com>
2023-07-28 22:36:35 +0900

    design pattern day1

--- src/main/java/org/example/designpattern/proxy/Proxy_Pattern.java
+++ src/main/java/org/example/designpattern/proxy/Proxy_Pattern.java
@@ -3,6 +3,8 @@ package org.example.designpattern.proxy;
 public class Proxy_Pattern {
     public static void main(String[] args) {
         ProxyImage image = new ProxyImage("TEST");
+        RealImage realImage = new RealImage("TEST_REAL");
+        realImage.display();
         image.display();
         image.display();
         image.display();
